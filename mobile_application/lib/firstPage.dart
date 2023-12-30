be0e7be6352e4862d86c09c051778245d260fb74
Perseus24 <105843665+Perseus24@users.noreply.github.com>
2023-12-30 20:50:46 +0800

    integrated firebase to main

--- mobile_application/lib/firstPage.dart
+++ mobile_application/lib/firstPage.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'temporarySecond.dart';
 import 'appColors.dart';
-void main(){
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future <void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
