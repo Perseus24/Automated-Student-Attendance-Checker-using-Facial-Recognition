6b0eab4b4fc53220676a865d6150cdafa2e767bc
cleofeerj <135526267+cleofeerj@users.noreply.github.com>
2024-04-25 22:38:06 +0800

    REVISED Profile Page - Delete the other one

--- tempo_profile.dart
+++ tempo_profile.dart
@@ -34,63 +34,78 @@ class Tempo_profile extends StatelessWidget {
               ),
             ),
           ),
-          Positioned(
-            top: getDynamicSize.getHeight(context) * 0.28,
-            child: Container(
-              height: getDynamicSize.getHeight(context),
-              width: getDynamicSize.getWidth(context),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: FractionalTranslation(
-                        translation:
-                            Offset(0.0, -0.60), // Adjust this value as needed
-                        child: Container(
-                          width: 120.w,
-                          height: 120.h,
-                          margin: EdgeInsets.only(bottom: 40),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.grey,
-                          ),
-                          child: Center(
-                            child: Icon(
-                              Icons.camera_alt_outlined,
-                              size: 40,
-                              color: Colors.white,
+          Container(
+            child: Positioned(
+              top: getDynamicSize.getHeight(context) * 0.28,
+              child: Container(
+                height: getDynamicSize.getHeight(context),
+                width: getDynamicSize.getWidth(context),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: FractionalTranslation(
+                          translation:
+                              Offset(0.0, -0.60), // Adjust this value as needed
+                          child: Container(
+                            width: 120.w,
+                            height: 120.h,
+                            margin: EdgeInsets.only(bottom: 40),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.grey,
+                            ),
+                            child: Center(
+                              child: Icon(
+                                Icons.camera_alt_outlined,
+                                size: 40,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    _buildProfileItem(
-                        'Name', 'Erjay Cleofe', CupertinoIcons.person),
-                    _buildProfileItem(
-                        'Course', 'BSCS-3A', CupertinoIcons.pencil),
-                    _buildProfileItem('Address', 'abc address, xyz city',
-                        CupertinoIcons.location),
-                    _buildProfileItem('Email', 'erjaybo.cleofe@bicol-u.edu.ph',
-                        CupertinoIcons.mail),
-                    SizedBox(height: 20),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          padding: EdgeInsets.all(15),
+                      _buildProfileItem(
+                          'Name', 'Erjay Cleofe', CupertinoIcons.person),
+                      SizedBox(height: 20.h),
+                      _buildProfileItem(
+                          'Course', 'BSCS-3A', CupertinoIcons.pencil),
+                      SizedBox(height: 20.h),
+                      _buildProfileItem('Address', 'abc address, xyz city',
+                          CupertinoIcons.location),
+                      SizedBox(height: 20.h),
+                      _buildProfileItem('Email',
+                          'erjaybo.cleofe@bicol-u.edu.ph', CupertinoIcons.mail),
+                      SizedBox(height: 40.h),
+                      Container(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.all(22),
+                            backgroundColor: kBlueColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                          child: Text(
+                            'Edit Profile',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 19,
+                                fontWeight: FontWeight.w700),
+                          ),
                         ),
-                        child: Text('Edit Profile'),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -105,7 +120,7 @@ class Tempo_profile extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 10),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(15),
         boxShadow: [
           BoxShadow(
             offset: Offset(0, 5),
@@ -119,7 +134,7 @@ class Tempo_profile extends StatelessWidget {
         title: Text(title),
         subtitle: Text(subtitle),
         leading: Icon(iconData),
-        trailing: Icon(Icons.arrow_forward, color: Colors.grey.shade400),
+        //trailing: Icon(Icons.arrow_forward, color: Colors.grey.shade400), // JUST UNCOMMENT FOR ARROW AT THE RIGHT SIDE
         tileColor: Colors.white,
       ),
     );
