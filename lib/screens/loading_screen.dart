6ba33a1c9960a36551890f9ef45324aaa87829d1
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-05-12 13:45:38 +0800

    task(home): Changed the UI design for the loading screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -8,6 +8,10 @@ import 'package:flutter_application_1/utilities/build_routes.dart';
 import 'package:flutter_application_1/utilities/constants.dart';
 import 'package:flutter_application_1/utilities/get_user_data.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:get/get.dart';
+
+import '../professor_side/main_homepage_prof.dart';
+import 'notification_page.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -18,7 +22,7 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  UserDataControllers userDataControllers = UserDataControllers();
+  UserDataControllers userDataControllers = Get.put(UserDataControllers());
 
   @override
   void initState()  {
@@ -35,19 +39,48 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getUserData() async{
     GetUserFirebaseInfo getUserFirebaseInfo = GetUserFirebaseInfo();
     await getUserFirebaseInfo.fetchStudentFirebaseInfos();
-    getUserFirebaseInfo.fetchNotificationHistory();
-    Navigator.push(context, createRouteGo(MainHomePage()));
+    if(userDataControllers.switchDashboardUser.value == 0){
+      getUserFirebaseInfo.fetchNotificationHistory();
+      Navigator.push(context, createRouteGo(MainHomePage()));
+    }else{
+      Navigator.push(context, createRouteGo(MainHomePageProf()));
+    }
+
 
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: LoadingAnimationWidget.discreteCircle(
-         color: kBlueColor,
-         size: 60,
-        secondRingColor: Colors.orange,
-        thirdRingColor: Colors.white30),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+                width: 150,
+                height: 80,
+                child: Stack(
+                    children: [
+                      Image.asset(
+                        'images/logo.png',
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                    ]
+                )
+            ),
+            SizedBox(height: 40,),
+            LoadingAnimationWidget.discreteCircle(
+             color: kBlueColor,
+             size: 60,
+            secondRingColor: Colors.orange,
+            thirdRingColor: Colors.white30),
+          ],
+        ),
       )
     );
   }
