7ff02661f4bda03a2fd552612d81e3ffab39f587
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-05-12 13:47:08 +0800

    task(home): Finished the UI for the profile page of students

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utilities/get_user_data.dart';
